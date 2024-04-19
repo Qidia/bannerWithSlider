8c6b72d00178d824aeff9264fa8671779b1a3093
Qidia <qidian_n@mail.ru>
2024-04-19 17:20:45 +0300

    Added logic
    added logic to familiarize with Swiper 9.2.4

---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
